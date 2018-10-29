0c03de7f60188d25334490739763e6f502829374
CyborgElf <42723440+CyborgElf@users.noreply.github.com>
2018-10-29 10:58:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 
+const Discord = require("discord.js");
+
+const TOKEN = "NTA0NDgwMzgwNzAzOTk3OTUz.DrivEA.w6gvk57oGSr5wck9gxrrZjP3u6Q";
+
+var bot = new Discord.Client();
+
+bot.on("ready", function(){
+    console.log("Ready");
+});
+
+bot.on("message", function(message) {
+    console.log(message.content);
+    if (message.author.equals(bot.user)) return;
+
+    if (message.content == "hello") {
+        message.channel.send("Hi, there!");
+    }
+});
+
+bot.login(TOKEN);
